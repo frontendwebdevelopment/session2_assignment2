0c203f7d0b92cf896b4e245b4f8c700f8f1e431c
Ajay Kumar S M <ajaymandakki@gmail.com>
2016-06-28 21:03:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function multiplication (num1, num2) {
+var nameIntro = "Multiplication Result is  ";
+var x = num1;
+var y = num2;
+var z = x * y;
+function result () {        
+console.log(nameIntro + z);
+}
+
+return result ();
+}
+multiplication ("20", "10");
